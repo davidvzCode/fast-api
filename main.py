dc1565fdb094920dd0106c89caabe0dea44c17f0
davidvzCode <davidvz.code@gmail.com>
2022-08-02 17:10:47 -0500

    validations_body

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 #Python
 from typing import Optional
 
+
 #Pydantic
 from pydantic import BaseModel
 
@@ -11,6 +12,11 @@ from fastapi import Body, Query, Path
 app = FastAPI()
 
 # Models
+
+class Location(BaseModel):
+    city: str
+    state: str
+    country: str
 
 class Person(BaseModel):
     first_name: str
@@ -59,3 +65,21 @@ def show_person(
     person_id: int = Path(..., gt=0)
 ):
     return {person_id: "It exists"}
+
+
+# Validations: Request Body
+
+@app.put("/person/{person_id}")
+def update_person(
+    person_id: int = Path(
+        ...,
+        gt = 0,
+        title = "Person ID",
+        description = "This is the person ID"
+    ),
+    person: Person = Body(...),
+    location: Location = Body(...)
+):
+    results = person.dict()
+    results.update(location.dict())
+    return results
